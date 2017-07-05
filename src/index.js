5a63cefd08387b8dac937830d8be1c76644d0116
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-07-05 12:13:02 +0300

    Code refactoring.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-/* eslint-disable import/default */
-
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
+
 import App from './containers/App';
 import configureStore from './store/configureStore';
-
 import './styles/styles.scss';
 
 const store = configureStore();
@@ -24,8 +22,8 @@ if (module.hot) {
   module.hot.accept('./containers/App', () => {
     const NewRoot = require('./containers/App').default;
     render(
-      <Provider>
-        <App store={store}/>
+      <Provider store={store}>
+        <App/>
       </Provider>,
       document.getElementById('app')
     );
