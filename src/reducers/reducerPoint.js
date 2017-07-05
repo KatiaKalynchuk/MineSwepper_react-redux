ee62c0c5cf4cb2d02e79c0387790a2b83dfdc3a0
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-07-05 12:14:38 +0300

    Code refactoring.

--- src/reducers/reducerPoint.js
+++ src/reducers/reducerPoint.js
@@ -1,5 +1,3 @@
-import {POINT} from '../constants/actionTypes';
-
 const initialState = {
     isMine: false,
     mineAround: 0, //мин рядом
@@ -9,14 +7,7 @@ const initialState = {
 };
 
 export default function reducerPointer(state = initialState, action) {
-  // let newState;
   switch (action.type) {
-    // case POINT:
-    //   newState = Object.assign({}, state);
-    //   newState.field[action.x][action.y] = {name: 1};
-    //   console.log(newState);
-    //   return newState;
-
     default:
       return state;
   }
