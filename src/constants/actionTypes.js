a139f653a9e9ae00cfe3bfb2573a99f4e10dbea7
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-07-05 11:53:08 +0300

    Added new action types.

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,6 +1,8 @@
 export const UPDATE_FIELD = 'UPDATE_FIELD';
 export const OPEN_CELL = 'OPEN_CELL';
 export const STATUS_LOSE = 'STATUS_LOSE';
+export const STATUS_WIN = 'STATUS_WIN';
 export const SET_FLAG = 'SET_FLAG';
 export const COUNT = 'COUNT';
 export const DIFFICULTY = 'DIFFICULTY';
+export const POINT = 'POINT';
