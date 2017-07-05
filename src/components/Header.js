0db5ce8ca0ce2bae53012372cead6035f109c880
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-07-05 11:57:42 +0300

    Code refactoring.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,36 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
-const Header = ({actions, fillField}) => {
+const Header = ({actions}) => {
 
-  const setDifficulty = (e) => {
-      let elValue = e.target.innerHTML;
-      switch(elValue) {
-          case 'Easy':
-              actions.setDifficulty(10, 10, 10);
-              break;
-          case 'Middle':
-              actions.setDifficulty(15, 15, 20);
-              break;
-          case 'Hard':
-              actions.setDifficulty(20, 20, 40);
-        }
-      fillField();
-  };
+   const setDifficulty = (e) => {
+       let elValue = e.target.innerHTML;
+       switch(elValue) {
+           case 'Easy':
+               actions.setDifficulty(10, 10, 10);
+               break;
+           case 'Normal':
+               actions.setDifficulty(15, 15, 20);
+               break;
+           case 'Hard':
+               actions.setDifficulty(20, 20, 40);
+       }
+   };
 
-  return (
-      <header className="header">
-          <h1>MineSwepper</h1>
-          <p>To start the game, choose the difficulty</p>
-          <div className="difficulty">
-              <button onClick={setDifficulty}>Easy</button>
-              <button onClick={setDifficulty}>Middle</button>
-              <button onClick={setDifficulty}>Hard</button>
-          </div>
-      </header>
-  );
+   return (
+       <header className="header">
+           <h1>MineSwepper</h1>
+           <p>To start the game, choose the difficulty</p>
+           <div className="difficulty">
+               <button onClick={setDifficulty}>Easy</button>
+               <button onClick={setDifficulty}>Normal</button>
+               <button onClick={setDifficulty}>Hard</button>
+           </div>
+       </header>
+   );
 };
 
 Header.propTypes = {
-    actions: PropTypes.object.isRequired,
-    fillField: PropTypes.func.isRequired
+    actions: PropTypes.object.isRequired
 };
 export default Header;
