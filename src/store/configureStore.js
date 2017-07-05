6f42915d15615ffcace5a8c30f0a7299c673e34f
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-07-05 12:20:35 +0300

    Code refactoring.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,36 +1,19 @@
-import {createStore, compose, applyMiddleware} from 'redux';
+import {createStore} from 'redux';
 
 import rootReducer from '../reducers';
 
-function configureStoreProd(initialState) {
-  const middlewares = [];
+function configureStore(initialState) {
 
-  return createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middlewares)
-    )
-  );
-}
+   const store = createStore(rootReducer, initialState);
+      if (module.hot) {
+          // Enable Webpack hot module replacement for reducers
+          module.hot.accept('../reducers', () => {
+            const nextReducer = require('../reducers').default; // eslint-disable-line global-require
+            store.replaceReducer(nextReducer);
+          });
+      }
 
-function configureStoreDev(initialState) {
-  const middlewares = [];
-
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-  const store = createStore(rootReducer, initialState, composeEnhancers(
-    applyMiddleware(...middlewares)
-    )
-  );
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('../reducers', () => {
-      const nextReducer = require('../reducers').default; // eslint-disable-line global-require
-      store.replaceReducer(nextReducer);
-    });
+      return store;
   }
-
-  return store;
-}
-
-const configureStore = process.env.NODE_ENV === 'production' ? configureStoreProd : configureStoreDev;
 
 export default configureStore;
